df516e9bf08025ed8229bfc6a74d5d09baec8304
ejemplo <inglaterra_dhoko@hotmail.com>
2018-09-27 04:46:47 -0500

    1 er comentario

---
+++ cangrejo.java
@@ -0,0 +1,32 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class cangrejo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class cangrejo extends Actor
+{
+    /**
+     * Act - do whatever the cangrejo wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+        super.move(4);
+        if(Greenfoot.isKeyDown("right"))
+        {
+            
+            turn(3);
+        }
+         if(Greenfoot.isKeyDown("left"))
+        {
+            
+            turn(-3);
+        }
+
+        
+    }    
+}
